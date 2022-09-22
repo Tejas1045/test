a4161020678d721645576b6cca59426ceff1a6a2
Tejas1045 <82049342+Tejas1045@users.noreply.github.com>
2022-09-22 16:43:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,3 +3,10 @@ resource "aws_instance" "foo" {
   ami           = "ami-08c40ec9ead489470"
   instance_type = "t2.micro"
 }
+
+resource "aws_sns_topic" "sns_topic" {
+    name = "Sns-topic"
+    fifo_topic = true
+    content_based_deduplication = true
+  
+}
