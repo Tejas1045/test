e4e592b49e5b0aa49a0594965aca1c8128cbad32
Tejas1045 <82049342+Tejas1045@users.noreply.github.com>
2022-09-22 17:35:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
-
+terraform {
+  backend "s3" {
+    bucket = "s3bucket454545"
+    key = "Test/"
+    region = "us-east-1"
+  }
+}
 resource "aws_instance" "foo" {
   ami           = "ami-08c40ec9ead489470"
   instance_type = "t2.micro"
