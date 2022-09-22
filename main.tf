bc135e7c3e12218eb0c9797e2278d341d0f4ed61
Tejas1045 <82049342+Tejas1045@users.noreply.github.com>
2022-09-22 16:37:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,5 @@
 
 resource "aws_instance" "foo" {
-  resource_name="main"
-  region="us-east-1"
-  ami           = ami-08c40ec9ead489470
+  ami           = "ami-08c40ec9ead489470"
   instance_type = "t2.micro"
 }
