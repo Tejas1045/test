cddb173d5757b011d94819f6da44b571a938d53c
Tejas1045 <82049342+Tejas1045@users.noreply.github.com>
2022-09-27 10:29:58 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
     region = "us-east-1"
-    access_key = "AKIA4DMVDGKGPJIG75OF"
+    access_key =${{ secrets.AWS_ACCESS_KEY_ID }}   #"AKIA4DMVDGKGPJIG75OF"
     secret_key = "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"
   
 }
